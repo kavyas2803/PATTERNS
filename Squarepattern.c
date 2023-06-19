56165f79a32ab2177fb8a010596980d64275803d
kavyas2803 <108827162+kavyas2803@users.noreply.github.com>
2023-06-19 19:52:30 +0530

    Create Squarepattern.c

---
+++ Squarepattern.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n,i,j;
+    printf("\n Enter the rows");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            
+            printf("*");
+            
+        }
+        printf("\n");
+    }
+    
+
+    return 0;
+}
