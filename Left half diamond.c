aae5c53db95a29d54f5ffb9e67f5318c0d0b7e41
kavyas2803 <108827162+kavyas2803@users.noreply.github.com>
2023-06-20 17:47:34 +0530

    Create Left half diamond.c

---
+++ Left half diamond.c
@@ -0,0 +1,37 @@
+#include <stdio.h>  
+  
+int main()  
+{  
+    int n,m=1;  
+    printf("Enter the number of rows");  
+    scanf("%d",&n);  
+    for(int i=n;i>=1;i--)  
+    {  
+        for(int j=1;j<=i-1;j++)  
+        {  
+          printf(" ");  
+        }  
+        for(int k=1;k<=m;k++)  
+        {  
+            printf("*");  
+        }  
+        printf("\n");  
+        m++;  
+    }  
+    m=n;
+     for(int i=1;i<=n;i++)  
+     {  
+       for(int j=1;j<i;j++)  
+       {  
+           printf(" ");  
+       }  
+       for(int k=1;k<=m;k++)  
+       {  
+           printf("*");  
+       }  
+       m--;  
+     
+      printf("\n");  
+    }  
+    return 0;  
+}  
